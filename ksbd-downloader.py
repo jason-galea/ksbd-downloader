8ac0e08a64fb00eee78ed3410b12c656d0edfe4e
JG <jasongalea98@outlook.com>
2023-02-13 23:22:05 +1100

    Doing work now, need to decide whether I CBF traversing the archives

--- ksbd-downloader.py
+++ ksbd-downloader.py
@@ -1,17 +1,56 @@
+#!/usr/bin/python3
+
+# import os
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import ui
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+
+def download_by_chapter(driver, url):
+    driver.get(url)
+    driver.find_element(By.LINK_TEXT, "Last »").click()
+
+    ### Loop through all pages of results (backwards, to sort chronologically)
+    while True:
+        
+        ### Get list of posts
+        posts = driver.find_elements(By.CLASS_NAME, "post-title")
+        posts.reverse() ### Again, reverse to get chronological order
+
+        for post in posts:
+            print(post.text)
+
+
+        # ### Get image URLs
+        # images = driver.find_elements(By.CLASS_NAME, "attachment-large size-large wp-post-image")
+
+        # for image in images:
+        #     print(image.get_attribute("src"))
+
+
+        ### Next page of posts
+        # os.wait(1)
+        driver.find_element(By.LINK_TEXT, "«").click()
+
+def download_by_index(driver, url):
+    driver.get(url)
+
+    # driver.find_element(By.LINK_TEXT, "Last »").click()
 
 def main():
-    driver = webdriver.Firefox()
-    driver.get('https://www.google.com/')
+    
+    BOOK_5_URL = "https://killsixbilliondemons.com/chapter/book-5-breaker-of-infinities/"
+    INDEX_URL = "https://killsixbilliondemons.com/wp-content/uploads/"
 
-    page_url=driver.find_elements_by_xpath("//a[@class='content']")
-    all_title = driver.find_elements_by_class_name("title")
-    title = [title.text for title in all_title]
+    ### Init
+    driver_options = Options()
+    driver_options.add_argument("--headless")
+    driver = webdriver.Firefox(
+        options=driver_options,
+    )
 
-    print(title)
-
+    ### Start navigating & downloading
+    # download_by_chapter(driver, BOOK_5_URL)
+    download_by_chapter(driver, INDEX_URL)
 
 if (__name__ == "__main__"):
     main()
