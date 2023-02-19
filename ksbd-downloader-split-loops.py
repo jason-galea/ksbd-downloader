efa95874122607851510975652c7a51f1fd9cde5
JG <jasongalea98@outlook.com>
2023-02-19 11:58:31 +1100

    Finished! :D (I think :D :D :D)

--- ksbd-downloader-split-loops.py
+++ ksbd-downloader-split-loops.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 
 import os
-import re
+import pickle
 from urllib import request
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
 
-class InvalidImageURLsFileException(Exception):
-    pass
-
-
+################################################################################################
+### FUNCTIONS
 def scrape_image_urls():
 
     ### Create webdriver
@@ -23,7 +21,7 @@ def scrape_image_urls():
     )
 
     ### Scrape image URLs
-    print(f"==> Scraping image URLs")
+    # print(f"==> Scraping image URLs")
     driver.get(FIRST_COMIC)
 
     image_urls = []
@@ -36,25 +34,26 @@ def scrape_image_urls():
             value="//img[contains(@src,'killsixbilliondemons.com/wp-content/uploads/')]",
         )
 
-        ### Save URLs to list
-        [print(i.get_attribute("src")) for i in img_elements]
-        image_urls += [i.get_attribute("src") for i in img_elements]
+        ### Save URLs to list (Without printing)
+        # image_urls += [i.get_attribute("src") for i in img_elements]
+
+        ### Save URLs to list (With printing)
+        temp_image_urls = [i.get_attribute("src") for i in img_elements]
+        for image_url in temp_image_urls:
+            print(f"==> Found image URL '{image_url}'")
+        image_urls += temp_image_urls
 
         ### Boilerplate
         if (driver.current_url != LAST_COMIC):
             driver.find_element(By.LINK_TEXT, "Next >").click()
         else:
-            # print(f"==> Reached LAST_COMIC: {LAST_COMIC}")
-            print(f"==> Scraped {len(image_urls)} URLs")
+            print(f"==> Finished scraping URLs")
             break
-
-    # print(f"==> DEBUG: image_urls = {image_urls}")
 
     ### Save image URLs to disk
     print(f"==> Saving image URLs to disk")
-    with open(IMAGE_URLS_FILE, "w") as f:
-        for image_url in image_urls:
-            f.write(f"{image_url}\n")
+    with open(IMAGE_URLS_FILE, "wb") as f:
+        pickle.dump(image_urls, f)
 
     return image_urls
 
@@ -83,22 +82,31 @@ def download_images(image_urls):
         # request.urlretrieve(image_url, filename)
 
 
-### Globals
+################################################################################################
+### GLOBALS
 ### NOTE: Obviously these test vars will give incorrect page number prefixes
 # FIRST_COMIC = "https://killsixbilliondemons.com/comic/king-of-swords-174-177-finale/"
 # FIRST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-9-to-1-10/"
 # FIRST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-37-to-1-38/"
 # FIRST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-3-82-to-3-83/"
-LAST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-9-to-1-10/"
+# LAST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-9-to-1-10/"
+# LAST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-37-to-1-38/"
 
 FIRST_COMIC = "https://killsixbilliondemons.com/comic/kill-six-billion-demons-breaker-of-infinities/"
-# LAST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-4-182-to-4-183/"
+LAST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-4-182-to-4-183/"
 
 CWD = os.getcwd()
-FIRST_COMIC_BASENAME = os.path.basename(FIRST_COMIC.removesuffix("/"))
-LAST_COMIC_BASENAME = os.path.basename(LAST_COMIC.removesuffix("/"))
+FIRST_COMIC_BASENAME = os.path.basename( FIRST_COMIC.removesuffix("/") ).replace("_", "-")
+LAST_COMIC_BASENAME = os.path.basename( LAST_COMIC.removesuffix("/") ).replace("_", "-")
 IMAGE_URLS_FILE = f"{CWD}/downloads/image_urls_{FIRST_COMIC_BASENAME}_to_{LAST_COMIC_BASENAME}.txt"
 DOWNLOAD_DIR = f"{CWD}/downloads/book-5-breaker-of-infinities"
+HELP_TEXT = f"""
+==> This script will download all pages from:
+==> \t'{FIRST_COMIC}
+==> To:
+==> \t'{LAST_COMIC}
+==> They will be downloaded to directory: '{DOWNLOAD_DIR}
+"""
 
 # print(f"DEBUG: LAST_COMIC = {LAST_COMIC}")
 # print(f"DEBUG: LAST_COMIC_BASENAME = {LAST_COMIC_BASENAME}")
@@ -108,15 +116,9 @@ DOWNLOAD_DIR = f"{CWD}/downloads/book-5-breaker-of-infinities"
 # print(f"DEBUG: DOWNLOAD_DIR = {DOWNLOAD_DIR}")
 
 
+################################################################################################
 def main():
-
-    ### Help text
-    # print(f"==> This script will download all pages from:")
-    # print(f"==> \t'{FIRST_COMIC}'")
-    # print(f"==> To:")
-    # print(f"==> \t'{LAST_COMIC}'")
-    # print(f"==> They will be downloaded to directory: '{DOWNLOAD_DIR}'")
-    # print()
+    print(HELP_TEXT)
 
     ### Check/Create dir
     if not (os.path.exists(DOWNLOAD_DIR)):
@@ -124,29 +126,18 @@ def main():
         os.makedirs(DOWNLOAD_DIR)
 
     ### Detect existing image_urls.txt file
-    try:
-        if not (os.path.exists(IMAGE_URLS_FILE)):
-            print(f"==> Did not detect existing IMAGE_URLS_FILE")
-            print(f"==> Continuing to scrape image URLs")
-            raise InvalidImageURLsFileException
+    if (os.path.exists(IMAGE_URLS_FILE)):
+        print(f"==> Reading URLs from existing file")
+        with open(IMAGE_URLS_FILE, "rb") as f:
+            image_urls = pickle.load(f)
 
-        ### Split filename to compare FIRST_COMIC and LAST_COMIC
-        image_urls_filename_split = (
-            os.path.basename(IMAGE_URLS_FILE)
-                .removeprefix("image_urls_")
-                .removesuffix(".txt")
-                .split("_to_")
-        )
-        # print(f"DEBUG: image_urls_filename_split = {image_urls_filename_split}")
-
-        for i, basename in enumerate(FIRST_COMIC_BASENAME, LAST_COMIC_BASENAME):
-            if (image_urls_filename_split[i] != basename):
-                print(f"==> Image URLs filename did not match FIRST_COMIC and/or LAST_COMIC")
-                print(f"==> Continuing to scrape image URLs")
-                raise InvalidImageURLsFileException
-        
-    except InvalidImageURLsFileException:
+    else:
+        print(f"==> Did not detect existing image URLs file")
+        print(f"==> Scraping image URLs")
         image_urls = scrape_image_urls()
+
+    # print(f"DEBUG: image_urls = {image_urls}")
+    print(f"==> Found {len(image_urls)} URLs")
 
     download_images(image_urls)
 
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+echo "$ sudo apt install -y firefox firefox-geckodriver"
+sudo apt install -y firefox firefox-geckodriver
