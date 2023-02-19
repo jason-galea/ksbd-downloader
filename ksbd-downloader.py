8ddd7efb666c461da8be05a4dc742d98c7b194e3
JG <jasongalea98@outlook.com>
2023-02-19 12:09:47 +1100

    feat: Cleaning up, deleted unused files

--- ksbd-downloader.py
+++ ksbd-downloader.py
@@ -1,42 +1,16 @@
 #!/usr/bin/python3
 
 import os
-import re
+import pickle
 from urllib import request
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
 
-
-def main():
-
-    ### Vars
-    ### NOTE: These extras are for testing only, using them will break the filename prefix
-    # FIRST_COMIC = "https://killsixbilliondemons.com/comic/king-of-swords-174-177-finale/"
-    # FIRST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-9-to-1-10/"
-    # FIRST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-37-to-1-38/"
-    # FIRST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-3-82-to-3-83/"
-
-    FIRST_COMIC = "https://killsixbilliondemons.com/comic/kill-six-billion-demons-breaker-of-infinities/"
-    LAST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-4-182-to-4-183/"
-    DOWNLOAD_DIR = "./downloads/book-5-breaker-of-infinities"
-    PAGE_NO = 0 ### "0" for the cover page
-    PAGE_NO_LENGTH = 4 ### "1-page" --> "0001-page"
-    
-
-    ### Help text
-    # print(f"==> This script will download all pages from:")
-    # print(f"==> \t'{FIRST_COMIC}'")
-    # print(f"==> To:")
-    # print(f"==> \t'{LAST_COMIC}'")
-    # print(f"==> They will be downloaded to directory: '{DOWNLOAD_DIR}'")
-    # print()
-
-    ### Check/Create dir
-    if not (os.path.exists(DOWNLOAD_DIR)):
-        print(f"==> Creating destination directory '{DOWNLOAD_DIR}'")
-        os.mkdir(DOWNLOAD_DIR)
+################################################################################################
+### FUNCTIONS
+def scrape_image_urls():
 
     ### Create webdriver
     print("==> Initialising webdriver")
@@ -46,48 +20,128 @@ def main():
         options=driver_options,
     )
 
-    ### Initial load
-    print(f"==> Loading FIRST_COMIC: '{FIRST_COMIC}'")
+    ### Scrape image URLs
+    # print(f"==> Scraping image URLs")
     driver.get(FIRST_COMIC)
 
-    ### Download
+    image_urls = []
     while True:
         print(f"==> Navigated to '{driver.current_url}'")
 
         ### Find image elements
-        imgs = driver.find_elements(
+        img_elements = driver.find_elements(
             by=By.XPATH,
             value="//img[contains(@src,'killsixbilliondemons.com/wp-content/uploads/')]",
         )
 
-        ### Download image(s)
-        for image_url in [i.get_attribute("src") for i in imgs]:
-            # print(f"==> Found image URL '{image_url}'")
-            
-            # filename = f"{DOWNLOAD_DIR}/{os.path.basename(image_url)}"
-            page_no_zfilled = str(PAGE_NO).zfill(PAGE_NO_LENGTH)
-            image_basename = os.path.basename(image_url)
-            filename = f"{DOWNLOAD_DIR}/{page_no_zfilled}-{image_basename}"
+        ### Save URLs to list (Without printing)
+        # image_urls += [i.get_attribute("src") for i in img_elements]
 
-            # if not (os.path.exists(filename)):
-            #     print(f"==> Downloading '{image_url}' to '{filename}'")
-            #     request.urlretrieve(image_url, filename)
-            # else: ### NOTE: Does this actually happen?
-            #     print(f"==> WARNING: File '{filename}' already exists")
-
-            print(f"==> Downloading '{image_url}' to '{filename}'")
-            request.urlretrieve(image_url, filename)
-
-            PAGE_NO += 1
-
+        ### Save URLs to list (With printing)
+        temp_image_urls = [i.get_attribute("src") for i in img_elements]
+        for image_url in temp_image_urls:
+            print(f"==> Found image URL '{image_url}'")
+        image_urls += temp_image_urls
 
         ### Boilerplate
         if (driver.current_url != LAST_COMIC):
             driver.find_element(By.LINK_TEXT, "Next >").click()
         else:
-            print(f"==> Reached LAST_COMIC: {LAST_COMIC}")
-            exit(0)
+            print(f"==> Finished scraping URLs")
+            break
 
+    ### Save image URLs to disk
+    print(f"==> Saving image URLs to disk")
+    with open(IMAGE_URLS_FILE, "wb") as f:
+        pickle.dump(image_urls, f)
+
+    return image_urls
+
+def download_images(image_urls):
+
+    ### Detect page no. zfill length
+    page_no_zfill_length = len(str( len(image_urls) - 1 )) ### -1 so cover page no. = 0
+    print(f"==> Detected page no. zfill length of {page_no_zfill_length}")
+
+    ### Download images
+    for page_no, image_url in enumerate(image_urls):
+        
+        # filename = f"{DOWNLOAD_DIR}/{os.path.basename(image_url)}"
+        page_no_zfilled = str(page_no).zfill(page_no_zfill_length)
+        image_basename = os.path.basename(image_url)
+        filename = f"{DOWNLOAD_DIR}/{page_no_zfilled}-{image_basename}"
+
+        ### Don't redownload images from previous runs
+        if not (os.path.exists(filename)):
+            print(f"==> Downloading '{image_url}' to '{os.path.basename(filename)}'")
+            request.urlretrieve(image_url, filename)
+        else:
+            print(f"==> WARNING: File '{os.path.basename(filename)}' already exists")
+
+        # print(f"==> Downloading '{image_url}' to '{os.path.basename(filename)}'")
+        # request.urlretrieve(image_url, filename)
+
+
+################################################################################################
+### GLOBALS
+### NOTE: Obviously these test vars will give incorrect page number prefixes
+# FIRST_COMIC = "https://killsixbilliondemons.com/comic/king-of-swords-174-177-finale/"
+# FIRST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-9-to-1-10/"
+# FIRST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-37-to-1-38/"
+# FIRST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-3-82-to-3-83/"
+# LAST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-9-to-1-10/"
+# LAST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-37-to-1-38/"
+
+FIRST_COMIC = "https://killsixbilliondemons.com/comic/kill-six-billion-demons-breaker-of-infinities/"
+LAST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-4-182-to-4-183/"
+
+CWD = os.getcwd()
+FIRST_COMIC_BASENAME = os.path.basename( FIRST_COMIC.removesuffix("/") ).replace("_", "-")
+LAST_COMIC_BASENAME = os.path.basename( LAST_COMIC.removesuffix("/") ).replace("_", "-")
+IMAGE_URLS_FILE = f"{CWD}/downloads/image_urls_{FIRST_COMIC_BASENAME}_to_{LAST_COMIC_BASENAME}.txt"
+DOWNLOAD_DIR = f"{CWD}/downloads/book-5-breaker-of-infinities"
+HELP_TEXT = f"""
+==> This script will download all pages from:
+==> \t'{FIRST_COMIC}
+==> To:
+==> \t'{LAST_COMIC}
+==> They will be downloaded to directory: '{DOWNLOAD_DIR}
+"""
+
+# print(f"DEBUG: LAST_COMIC = {LAST_COMIC}")
+# print(f"DEBUG: LAST_COMIC_BASENAME = {LAST_COMIC_BASENAME}")
+# print(f"DEBUG: FIRST_COMIC = {FIRST_COMIC}")
+# print(f"DEBUG: FIRST_COMIC_BASENAME = {FIRST_COMIC_BASENAME}")
+# print(f"DEBUG: IMAGE_URLS_FILE = {IMAGE_URLS_FILE}")
+# print(f"DEBUG: DOWNLOAD_DIR = {DOWNLOAD_DIR}")
+
+
+################################################################################################
+def main():
+    print(HELP_TEXT)
+
+    ### Check/Create dir
+    if not (os.path.exists(DOWNLOAD_DIR)):
+        print(f"==> Creating download directory '{DOWNLOAD_DIR}'")
+        os.makedirs(DOWNLOAD_DIR)
+
+    ### Detect existing image_urls.txt file
+    if (os.path.exists(IMAGE_URLS_FILE)):
+        print(f"==> Reading URLs from existing file")
+        with open(IMAGE_URLS_FILE, "rb") as f:
+            image_urls = pickle.load(f)
+
+    else:
+        print(f"==> Did not detect existing image URLs file")
+        print(f"==> Scraping image URLs")
+        image_urls = scrape_image_urls()
+
+    # print(f"DEBUG: image_urls = {image_urls}")
+    print(f"==> Found {len(image_urls)} URLs")
+
+    download_images(image_urls)
+
+    print(f"==> ALL DONE!!!!")
 
 
 if (__name__ == "__main__"):
