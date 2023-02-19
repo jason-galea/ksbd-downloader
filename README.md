56b0b0214926b83eedea9db86408935f1278f27a
JG <jasongalea98@outlook.com>
2023-02-19 11:05:59 +1100

    feat: Save image_urls.txt to avoid re-scraping the same URLs, update README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ksbd-downloader
-Scripts to download webcomic "Kill Six Billion Demons"
+Scripts to download webcomic "Kill Six Billion Demons".
+
+This script uses Selenium to fetch image URLs from "https://killsixbilliondemons.com", then download them.
+
+# Features
+- Detect existing images from previous runs
+- Detect existing image URLs from previous runs
+
+# Using the script
+
+Statically set "FIRST_COMIC" and "LAST_COMIC" variables with relevant URLs.
+
+Then, simply execute from the base repository dir:
+
+    ./ksbd-downloader.py
--- ksbd-downloader-split-loops.py
+++ ksbd-downloader-split-loops.py
@@ -8,7 +8,6 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
 
-
 def main():
 
     ### Vars
@@ -17,13 +16,16 @@ def main():
     # FIRST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-9-to-1-10/"
     # FIRST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-37-to-1-38/"
     # FIRST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-3-82-to-3-83/"
+    LAST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-1-9-to-1-10/"
 
     FIRST_COMIC = "https://killsixbilliondemons.com/comic/kill-six-billion-demons-breaker-of-infinities/"
-    LAST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-4-182-to-4-183/"
-    DOWNLOAD_DIR = f"{os.getcwd()}/downloads/book-5-breaker-of-infinities"
-    
-    # PAGE_NO = 0 ### "0" for the cover page
-    # PAGE_NO_LENGTH = 4 ### "1-page" --> "0001-page"
+    # LAST_COMIC = "https://killsixbilliondemons.com/comic/breaker-of-infinities-4-182-to-4-183/"
+
+    CWD = os.getcwd()
+    FIRST_COMIC_BASENAME = os.path.basename(FIRST_COMIC)
+    LAST_COMIC_BASENAME = os.path.basename(LAST_COMIC)
+    IMAGE_URLS_FILE = f"{CWD}/downloads/image_urls_{FIRST_COMIC_BASENAME}_to_{LAST_COMIC_BASENAME}.txt"
+    DOWNLOAD_DIR = f"{CWD}/downloads/book-5-breaker-of-infinities"
     
 
     ### Help text
@@ -47,6 +49,24 @@ def main():
         options=driver_options,
     )
 
+    ### Detect existing image_urls.txt file
+    if (os.path.exists(IMAGE_URLS_FILE)):
+        print(f"==> Detected existing image URLs file '{IMAGE_URLS_FILE}'")
+
+        ### Split filename to compare FIRST_COMIC and LAST_COMIC
+        image_urls_filename_split = os.path.basename(IMAGE_URLS_FILE).removeprefix("image_urls_").split("_to_")
+        
+        if (image_urls_filename_split[0] != FIRST_COMIC_BASENAME):
+            print(f"==> FIRST_COMIC from image URLs filename did not match, continuing to scrape image URLs")
+            pass
+        if (image_urls_filename_split[1] != LAST_COMIC_BASENAME):
+            print(f"==> LAST_COMIC from image URLs filename did not match, continuing to scrape image URLs")
+            pass
+
+    else:
+        print(f"==> Did not detect existing IMAGE_URLS_FILE, continuing to scrape image URLs")
+        pass
+
     ### Initial load
     print(f"==> Loading FIRST_COMIC: '{FIRST_COMIC}'")
     driver.get(FIRST_COMIC)
@@ -64,6 +84,7 @@ def main():
         )
 
         ### Save URLs to list
+        [print(i.get_attribute("src")) for i in img_elements]
         image_urls += [i.get_attribute("src") for i in img_elements]
 
         ### Boilerplate
@@ -74,10 +95,15 @@ def main():
             print(f"==> Scraped {len(image_urls)} URLs")
             break
 
-    print(f"==> DEBUG: image_urls = {image_urls}")
+    # print(f"==> DEBUG: image_urls = {image_urls}")
+
+    ### Save image URLs to disk
+    print(f"==> Saving image URLs to disk")
+    with open(IMAGE_URLS_FILE, "w") as f:
+        f.writelines(image_urls)
 
     ### Detect page no. zfill length
-    page_no_zfill_length = len(image_urls) - 1 ### -1 to make the cover page no. = 0
+    page_no_zfill_length = len(str( len(image_urls) - 1 )) ### -1 so cover page no. = 0
     print(f"==> Detected page no. zfill length of {page_no_zfill_length}")
 
     ### Download images
@@ -88,13 +114,14 @@ def main():
         image_basename = os.path.basename(image_url)
         filename = f"{DOWNLOAD_DIR}/{page_no_zfilled}-{image_basename}"
 
-        # if not (os.path.exists(filename)):
-        #     print(f"==> Downloading '{image_url}' to '{filename}'")
-        #     request.urlretrieve(image_url, filename)
-        # else: ### NOTE: Does this actually happen?
-        #     print(f"==> WARNING: File '{filename}' already exists")
+        ### Don't redownload images from previous runs
+        if not (os.path.exists(filename)):
+            print(f"==> Downloading '{image_url}' to '{os.path.basename(filename)}'")
+            request.urlretrieve(image_url, filename)
+        else:
+            print(f"==> WARNING: File '{filename}' already exists")
 
-        print(f"==> Downloading '{image_url}' to '{os.path.basename(filename)}'")
+        # print(f"==> Downloading '{image_url}' to '{os.path.basename(filename)}'")
         # request.urlretrieve(image_url, filename)
 
 
