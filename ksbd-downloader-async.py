78947299bebb80cecda011f5007f8dffed2968ad
Jason Galea <jasonpgalea@outlook.com>
2023-11-05 12:54:12 +1100

    bye bye workflows

--- ksbd-downloader-async.py
+++ ksbd-downloader-async.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 
-
-from urllib.request import urlretrieve
-from urllib.parse import urlparse
-from glob import glob
-
 import os
 import json
 import click
+# import urllib
+# import pickle
+# import argparse
+import httpx
+import asyncio
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
+from urllib.request import urlretrieve
+from urllib.parse import urlparse
+from glob import glob
 
 GET_IMAGE_WORKER_COUNT = 4
 BOOKS_INFO: list = [
@@ -210,6 +213,34 @@ def main(
             with open(chapter_details_file, "r", encoding="utf8") as f:
                 chapter_details = json.load(f)
 
+        
+        # if (force_get_details) and (os.path.exists(chapter_details_file)):
+        #     print(f"==> INFO: 'force_get_details' enabled, removing '{os.path.basename(chapter_details_file)}'")
+        #     os.remove(chapter_details_file)
+
+        # if not dont_get_details:
+
+        #     if (not os.path.exists(chapter_details_file)) or (force_get_details):
+        #         print(f"\n==> INFO: Begin downloading page details for book {book}, chapter {c+1}")
+
+        #         chapter_details = get_chapter_details(driver, **BOOKS_INFO[book-1]["chapters"][c])
+
+        #         print(f"==> INFO: Finished downloading page details for book {book}, chapter {c+1}")
+        #         print(f"==> INFO: Writing chapter details to '{chapter_details_file}'")
+
+        #         with open(chapter_details_file, "w", encoding="utf8") as f:
+        #             f.write(standard_json_dumps(chapter_details))
+            
+        #     elif not force_get_details:
+
+
+        #     else:
+        #         print(f"==> INFO: Chapter details file '{chapter_details_file}' already exists")
+            
+
+        # else:
+        #     print(f"==> INFO: Skipped downloading chapter details due to 'dont_get_details'")
+
 
         # # print(f"==> DEBUG: chapter_details = {json.dumps(chapter_details, indent=4, ensure_ascii=False)}")
         # print(f"==> DEBUG: chapter_details = {standard_json_dumps(chapter_details)}")
@@ -230,6 +261,23 @@ def main(
 
 
             get_chapter_images(BOOK_DIR, chapter_details, c)
+            # asyncio.run(get_chapter_images_async(BOOK_DIR, chapter_details, c))
+
+
+            # ##################################################################
+            # ### PROFILING
+            # import cProfile
+            # import pstats
+
+            # with cProfile.Profile() as pr:
+            #     # get_chapter_images(BOOK_DIR, chapter_details, c)
+            #     get_chapter_images_async(BOOK_DIR, chapter_details, c)
+            
+            # stats = pstats.Stats(pr)
+            # stats.sort_stats(pstats.SortKey.TIME)
+            # stats.print_stats()
+            # ### PROFILING
+            # ##################################################################
 
 
             print(f"==> INFO: Finished downloading images for book {book}, chapter {c+1}")
@@ -295,6 +343,135 @@ def get_chapter_details(driver: webdriver.Firefox, start_url: str, end_url: str)
     return chapter_details
 
 
+# async def get_chapter_images_async(dir: str, chapter_details: list, chapter_no: int) -> None:
+
+#     queue = asyncio.Queue()
+#     existing_images = glob(f"{dir}/{chapter_no+1}-*.jpg")
+
+
+#     async with httpx.AsyncClient() as client:
+
+#         tasks = []
+
+#         ### Generate work items (data required for a worker to know how to perform a task)
+#         for p, page in enumerate(chapter_details):
+#             for i, image_url in enumerate(page["image_urls"]):
+
+#                 image_url_parsed = urlparse(image_url)
+#                 original_image_file = os.path.basename(image_url_parsed.path)
+
+#                 # image_file = f"{dir}/{chapter_no+1}-{str(p).zfill(2)}-{original_image_file}"
+#                 # image_file = f"{dir}/c{chapter_no+1}-p{str(p).zfill(2)}-i{i}-{original_image_file}" ### "c1-p00-i0-ksbdcoverchapter1.jpg"
+#                 image_file = f"{dir}/{chapter_no+1}-{str(p).zfill(2)}-{i}-{original_image_file}" ### "1-00-0-ksbdcoverchapter1.jpg"
+
+#                 # if not os.path.exists(image_file):
+#                 if not image_file in existing_images:
+#                     # queue.put_nowait(urlretrieve(image_url, image_file))
+#                     queue.put_nowait((image_url, image_file))
+
+#                 #     print(f"==> INFO: Downloaded image '{os.path.basename(image_file)}'")
+#                 # else:
+#                 #     print(f"==> INFO: Image '{os.path.basename(image_file)}' already exists")
+    
+
+#         ### Create worker tasks
+#         # tasks = [asyncio.create_task(get_chapter_images_worker(i)) for i in range(GET_IMAGE_WORKER_COUNT)]
+#         tasks = [asyncio.create_task(get_chapter_images_worker(queue, client)) for _ in range(GET_IMAGE_WORKER_COUNT)]
+
+#         ### Wait until queue is processed
+#         await queue.join()
+
+#         ### Cancel worker tasks
+#         for task in tasks:
+#             task.cancel()
+
+#         ### Wait for all worker tasks to be cancelled
+#         await asyncio.gather(*tasks, return_exceptions=True)
+
+
+# async def get_chapter_images_worker(queue: asyncio.Queue, client: httpx.AsyncClient):
+#     while True:
+
+#         ### Get a "work item" from queue
+#         url_file_tuple = await queue.get()
+#         url = url_file_tuple[0]
+#         file = url_file_tuple[1]
+
+#         ### Execute the work
+#         # urlretrieve(*url_file_tuple) ### E.G: urlretrieve(url_file_tuple[0], url_file_tuple[1])
+#         image_response = client.get(url)
+#         with open(file) as f:
+#             f.write(image_response.content)
+
+#         ### Notify queue
+#         queue.task_done()
+
+#         print(f"==> INFO: Downloaded image '{os.path.basename(url_file_tuple[1])}'")
+
+
+# async def get_chapter_images_async(dir: str, chapter_details: list, chapter_no: int) -> None:
+
+#     queue = asyncio.Queue()
+#     existing_images = glob(f"{dir}/{chapter_no+1}-*.jpg")
+
+#     ### Generate work items (data required for a worker to know how to perform a task)
+#     for p, page in enumerate(chapter_details):
+#         for i, image_url in enumerate(page["image_urls"]):
+
+#             image_url_parsed = urlparse(image_url)
+#             original_image_file = os.path.basename(image_url_parsed.path)
+
+#             # image_file = f"{dir}/{chapter_no+1}-{str(p).zfill(2)}-{original_image_file}"
+#             # image_file = f"{dir}/c{chapter_no+1}-p{str(p).zfill(2)}-i{i}-{original_image_file}" ### "c1-p00-i0-ksbdcoverchapter1.jpg"
+#             image_file = f"{dir}/{chapter_no+1}-{str(p).zfill(2)}-{i}-{original_image_file}" ### "1-00-0-ksbdcoverchapter1.jpg"
+
+#             # if not os.path.exists(image_file):
+#             if not image_file in existing_images:
+#                 # queue.put_nowait(urlretrieve(image_url, image_file))
+#                 queue.put_nowait((image_url, image_file))
+
+#             #     print(f"==> INFO: Downloaded image '{os.path.basename(image_file)}'")
+#             # else:
+#             #     print(f"==> INFO: Image '{os.path.basename(image_file)}' already exists")
+    
+#     async with httpx.AsyncClient() as client:
+
+#         ### Create worker tasks
+#         # tasks = [asyncio.create_task(get_chapter_images_worker(i)) for i in range(GET_IMAGE_WORKER_COUNT)]
+#         tasks = [asyncio.create_task(get_chapter_images_worker(queue, client)) for _ in range(GET_IMAGE_WORKER_COUNT)]
+
+#         ### Wait until queue is processed
+#         await queue.join()
+
+#         ### Cancel worker tasks
+#         for task in tasks:
+#             task.cancel()
+
+#         ### Wait for all worker tasks to be cancelled
+#         await asyncio.gather(*tasks, return_exceptions=True)
+
+
+# async def get_chapter_images_worker(queue: asyncio.Queue, client: httpx.AsyncClient):
+#     while True:
+
+#         ### Get a "work item" from queue
+#         url_file_tuple = await queue.get()
+#         url = url_file_tuple[0]
+#         file = url_file_tuple[0]
+
+#         ### Execute the work
+#         # urlretrieve(*url_file_tuple) ### E.G: urlretrieve(url_file_tuple[0], url_file_tuple[1])
+#         image_response = client.get(url)
+#         with open(file) as f:
+#             f.write(image_response.content)
+
+#         ### Notify queue
+#         queue.task_done()
+
+#         print(f"==> INFO: Downloaded image '{os.path.basename(url_file_tuple[1])}'")
+
+
+
 def get_chapter_images(dir: str, chapter_details: list, chapter_no: int) -> None:
     for p, page in enumerate(chapter_details):
         for i, image_url in enumerate(page["image_urls"]):
@@ -316,3 +493,14 @@ def get_chapter_images(dir: str, chapter_details: list, chapter_no: int) -> None
 
 if (__name__ == "__main__"):
     main()
+
+
+    # import cProfile
+    # import pstats
+
+    # with cProfile.Profile() as pr:
+    #     main()
+    
+    # stats = pstats.Stats(pr)
+    # stats.sort_stats(pstats.SortKey.TIME)
+    # stats.print_stats()
